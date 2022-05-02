56f29e316785dbf37e89470849348acbe4f73549
lautarog1596 <lautarog1596@gmail.com>
2022-05-02 13:30:00 -0300

    Metric-Imperial Converter

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,35 @@
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+module.exports = (app) => {
+
+    let convertHandler = new ConvertHandler();
+
+    app.route('/api/convert')
+        .get((req, res) => {
+            let input = req.query.input;
+            let initNum = convertHandler.getNum(input);
+            let initUnit = convertHandler.getUnit(input);
+
+            if (!initNum && !initUnit) {
+                res.send('invalid number and unit');
+            } else if (!initNum) {
+                res.send('invalid number');
+            } else if (!initUnit) {
+                res.send('invalid unit');
+            }
+
+            let returnNum = convertHandler.convert(initNum, initUnit);
+            let returnUnit = convertHandler.getReturnUnit(initUnit);
+            let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+            res.json({
+                initNum: initNum,
+                initUnit: initUnit,
+                returnNum: returnNum,
+                returnUnit: returnUnit,
+                string: toString
+            });
+        });
 
 };
